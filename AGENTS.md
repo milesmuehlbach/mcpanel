8e13baee7b4b6324f279442900acfd64089d0ca1
milesmuehlbach <muehlbachmiles@gmail.com>
2026-03-20 01:26:31 -0500

    add small sidebar for testing and modify agents.md

--- AGENTS.md
+++ AGENTS.md
@@ -29,3 +29,5 @@ You MUST use this tool whenever writing Svelte code before sending it to the use
 
 Generates a Svelte Playground link with the provided code.
 After completing the code, ask the user if they want a playground link. Only call this tool after user confirmation and NEVER if code was written to files in their project.
+
+# This project is using Shadcn-svelte ui components and general design. you can access LLM documentation for it at https://www.shadcn-svelte.com/llms.txt
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import PanelSidebar from "$lib/components/panel-sidebar.svelte";
+
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-
-<main class="max-w-6xl mx-auto px-4 py-6">
-	{@render children()}
-</main>
+<Sidebar.Provider>
+	<PanelSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
