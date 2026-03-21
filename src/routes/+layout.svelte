dfa94cdfad9d8b992fee41ecb6cdbbdbdce9a349
milesmuehlbach <muehlbachmiles@gmail.com>
2026-03-21 14:56:37 -0500

    small fixes, plus toasts for successful registration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-	<main>
-		{@render children?.()}
-	</main>
+<Toaster />
+<main>
+	{@render children?.()}
+</main>
