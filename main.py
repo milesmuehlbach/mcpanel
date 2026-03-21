67219ec9f7d45656ce4cd5dda1539f0ec73e51b4
technodot <techdudie@mail.com>
2026-03-21 15:30:39 -0500

    feat: make hot reload an argument sbux vibes peak ftr btw

--- main.py
+++ main.py
@@ -18,8 +18,14 @@ if __name__ == "__main__":
         default=pathlib.Path("./minecraft"),
         help="path to the working directory (default: ./minecraft)",
     )
+    parser.add_argument(
+        "--dev",
+        type=bool,
+        default=False,
+        help="run in development mode with hot reloading (default: False)",
+    )
     args = parser.parse_args()
 
     os.environ[APP_PATH_ENV] = str(args.path.resolve())
 
-    uvicorn.run("main:app_factory", host="0.0.0.0", port=8080, reload=True, factory=True)
+    uvicorn.run("main:app_factory", host="0.0.0.0", port=8080, workers=1, reload=args.dev, factory=True) # WORKERS HAS TO BE 1 BC I'M A FUCKING DUMBASS
