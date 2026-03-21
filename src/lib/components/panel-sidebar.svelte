797ed9325d55e7e7994636dd3b0de28f2c2eb479
milesmuehlbach <muehlbachmiles@gmail.com>
2026-03-21 00:02:02 -0500

    fix: vite build error

--- src/lib/components/panel-sidebar.svelte
+++ src/lib/components/panel-sidebar.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import ServerSelector from '$lib/components/serverselection.svelte';
+	import favicon from '$lib/assets/favicon.svg';
 </script>
 
 <Sidebar.Root>
 	<Sidebar.Header>
 		<h1 class="left-5 text-2xl font-extrabold">McPanel</h1>
-		<ServerSelector servertitles={['Server 1']} iconpath={['/favicon.png']} />
+		<ServerSelector servertitles={['Server 1']} iconpath={[favicon]} />
 	</Sidebar.Header>
 	<Sidebar.Content>
 		<Sidebar.Group />
--- src/lib/components/serverselection.svelte
+++ src/lib/components/serverselection.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import CheckIcon from '@lucide/svelte/icons/check';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
 	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
-	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
-	let { servertitles = [], iconpath = [] }: { servertitles: string[]; iconpath: string[] } =
-		$props();
+	import PlusIcon from '@lucide/svelte/icons/plus';
+
+	let {
+		servertitles = [],
+		iconpath = []
+	}: {
+		servertitles: string[];
+		iconpath: string[];
+	} = $props();
+
+	const sidebar = useSidebar();
 
 	let selectedIndex = $state(0);
 
-	let displayTitle = $derived(servertitles[selectedIndex] ?? 'No Server Selected');
-	let displayIcon = $derived(iconpath[selectedIndex] ?? 'none');
+	let activeTitle = $derived(servertitles[selectedIndex] ?? 'No Server');
+	let activeIcon = $derived(iconpath[selectedIndex] ?? '');
 </script>
 
 <Sidebar.Menu>
@@ -19,35 +27,56 @@
 			<DropdownMenu.Trigger>
 				{#snippet child({ props })}
 					<Sidebar.MenuButton
+						{...props}
 						size="lg"
 						class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-						{...props}
 					>
 						<div
 							class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
 						>
-							{#if displayIcon && displayIcon !== 'none'}
-								<img src={displayIcon} alt="Server Icon" class="h-5 w-5 rounded-sm object-cover" />
+							{#if activeIcon}
+								<img src={activeIcon} alt={activeTitle} class="size-4" />
 							{:else}
-								<GalleryVerticalEndIcon class="h-5 w-5" />
+								<div class="size-4 rounded-sm bg-muted"></div>
 							{/if}
 						</div>
-						<div class="flex flex-col gap-0.5 leading-none">
-							<span class="font-semibold">{displayTitle}</span>
+						<div class="grid flex-1 text-start text-sm leading-tight">
+							<span class="truncate font-medium">
+								{activeTitle}
+							</span>
+							<span class="truncate text-xs">Minecraft Server</span>
 						</div>
 						<ChevronsUpDownIcon class="ms-auto" />
 					</Sidebar.MenuButton>
 				{/snippet}
 			</DropdownMenu.Trigger>
-			<DropdownMenu.Content class="w-(--bits-dropdown-menu-anchor-width)" align="start">
-				{#each servertitles as version, i (version)}
-					<DropdownMenu.Item onSelect={() => (selectedIndex = i)}>
-						{version}
-						{#if i === selectedIndex}
-							<CheckIcon class="ms-auto" />
-						{/if}
+			<DropdownMenu.Content
+				class="w-(--bits-dropdown-menu-anchor-width) min-w-56 rounded-lg"
+				align="start"
+				side={sidebar.isMobile ? 'bottom' : 'right'}
+				sideOffset={4}
+			>
+				<DropdownMenu.Label class="text-xs text-muted-foreground">Servers</DropdownMenu.Label>
+				{#each servertitles as title, index (title)}
+					<DropdownMenu.Item onSelect={() => (selectedIndex = index)} class="gap-2 p-2">
+						<div class="flex size-6 items-center justify-center rounded-md border">
+							{#if iconpath[index]}
+								<img src={iconpath[index]} alt={title} class="size-3.5 shrink-0" />
+							{:else}
+								<div class="size-3.5 rounded-sm bg-muted"></div>
+							{/if}
+						</div>
+						{title}
+						<DropdownMenu.Shortcut>⌘{index + 1}</DropdownMenu.Shortcut>
 					</DropdownMenu.Item>
 				{/each}
+				<DropdownMenu.Separator />
+				<DropdownMenu.Item class="gap-2 p-2">
+					<div class="flex size-6 items-center justify-center rounded-md border bg-transparent">
+						<PlusIcon class="size-4" />
+					</div>
+					<div class="font-medium text-muted-foreground">Add server</div>
+				</DropdownMenu.Item>
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
 	</Sidebar.MenuItem>
